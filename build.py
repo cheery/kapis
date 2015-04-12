65a0985e858789a17c793e1d301886207263e244
Henri Tuhola <cheery@boxbase.org>
2015-04-13 00:30:26 +0300

    add -w to build script

--- build.py
+++ build.py
@@ -1,13 +1,41 @@
-from subprocess import check_output
-import os
+from subprocess import check_output, CalledProcessError
+import datetime, os, sys, time
 
-output = "window.kapis = {};\n"
+source_directory = 'src'
+outfile = "kapis.js"
+mtimes = {}
 
-for base, dirs, fils in os.walk('src'):
-    for filename in fils:
-        if filename.endswith('.coffee'):
-            path = os.path.join(base, filename)
-            output += check_output(['coffee', '-p', path])
+def build():
+    output = "window.kapis = {};\n"
+    for path in coffee_files():
+        mtimes[path] = os.path.getmtime(path)
+        output += check_output(['coffee', '-p', path])
+    with open(outfile, 'w') as fd:
+        fd.write(output)
 
-with open("kapis.js", 'w') as fd:
-    fd.write(output)
+def watch():
+    while True:
+        mtime = os.path.getmtime(outfile)
+        time.sleep(0.5)
+        rebuild = False
+        for path in coffee_files():
+            mtime = os.path.getmtime(path)
+            wtime = mtimes.get(path, mtime-1)
+            if wtime < mtime:
+                rebuild = True
+        if rebuild:
+            print "%s: rebuild" % datetime.datetime.utcnow()
+            try:
+                build()
+            except CalledProcessError as error:
+                print error
+
+def coffee_files():
+    for base, dirs, fils in os.walk(source_directory):
+        for filename in fils:
+            if filename.endswith('.coffee'):
+                yield os.path.join(base, filename)
+
+build()
+if len(sys.argv) > 0 and sys.argv[1] in ('-w', '--watch'):
+    watch()
